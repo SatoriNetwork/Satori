06edf33d640dc6901bcd08283937e296b77a938b
Jordan Miller <Jordan.kay@gmail.com>
2022-03-06 09:21:16 -0700

    #5 made simple subscriber app for testing streamr light client integration

--- node/app.js
+++ node/app.js
@@ -1,3 +1,4 @@
+const { StreamrClient } = require('streamr-client');
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -12,3 +13,23 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+async function subscribeTest() {
+    // not sensitive
+    const privateKey = 'a8b2f15fa298f086e3fd80990bf0380184f90a7c9dce738dd3e3418e2892ff54'
+    const streamr = new StreamrClient({
+        auth: {
+            privateKey: privateKey,
+        },
+    })
+    const streamId = 'binance-streamr.eth/ETHUSDT/ticker'
+    //const streamId = 'streamr.eth/demos/twitter/sample'
+    const subscription = await streamr.subscribe({
+        stream: streamId,
+    },
+    (message) => {
+        // This function will be called when new messages occur
+        console.log(JSON.stringify(message))
+    })
+}
+subscribeTest();
--- node/example/node-js-executable-master/README.md
+++
@@ -1,8 +0,0 @@
-# node-js-executable
-A boilerplate to create JavaScript binary executable files using Node.
-
-# build
-```
-$ npm i -g pkg
-$ pkg --output build/web .
-```
