8adbf4456dc2c236df8c6bb04643c06ca750e536
Oskar Laska <ooskar1233@gmail.com>
2022-08-12 23:32:00 +0200

    Add PubSub.Publish test for error flow

--- server/satori/test/satori/pub_sub/publish_test.exs
+++ server/satori/test/satori/pub_sub/publish_test.exs
@@ -7,12 +7,12 @@ defmodule Satori.PubSub.PublishTest do
 
   setup :verify_on_exit!
 
+  @mock_topic "test topic"
+  @mock_observation %{"some" => "test", "data" => "here"}
+
+  @mock_input %Input{topic: @mock_topic, observation: @mock_observation}
+
   describe "publish/1" do
-    @mock_topic "test topic"
-    @mock_observation %{"some" => "test", "data" => "here"}
-
-    @mock_input %Input{topic: @mock_topic, observation: @mock_observation}
-
     @publisher_return :ok
 
     test "calls Publisher.publish/2 with topic and observation from %Input{}" do
@@ -34,6 +34,20 @@ defmodule Satori.PubSub.PublishTest do
       end)
 
       assert %Output{error: nil} == Publish.publish(@mock_input)
+    end
+  end
+
+  describe "publish/1 when Publisher returns error" do
+    @mock_error :some_important_reason
+    @publisher_return {:error, @mock_error}
+
+    test "returns %Output{} with the same error" do
+      PublisherMock
+      |> expect(:publish, fn _, _ ->
+        @publisher_return
+      end)
+
+      assert %Output{error: @mock_error} == Publish.publish(@mock_input)
     end
   end
 end
