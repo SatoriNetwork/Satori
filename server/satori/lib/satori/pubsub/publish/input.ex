1322ce469eec0844e9710bd9b2966d5b4327ad23
Oskar Laska <ooskar1233@gmail.com>
2022-08-15 11:45:26 +0200

    Minor formatting

--- server/satori/lib/satori/pubsub/publish/input.ex
+++ server/satori/lib/satori/pubsub/publish/input.ex
@@ -2,7 +2,7 @@ defmodule Satori.PubSub.Publish.Input do
   defstruct [:topic, :observation]
 
   @type t :: %__MODULE__{
-    topic: binary(),
-    observation: map()
+          topic: binary(),
+          observation: map()
         }
 end
