b72884bab97cacd54143c26aebe8acebb175635a
Jordan Miller <Jordan.kay@gmail.com>
2022-08-26 22:52:44 -0600

    #51 not using auth payload at this time

--- node/satori/lib/apis/server/mock_use_client.py
+++ node/satori/lib/apis/server/mock_use_client.py
@@ -5,7 +5,7 @@ from satori.lib.apis.server import ClientConnection
 def establishConnection(wallet: wallet.Wallet):
     ''' establishes a connection to the satori server, returns connection object '''
     connectionPayload = wallet.payload()
-    print(f'establishing a connection with payload: {connectionPayload}')
+    print(f'establishing a connection...')# with payload: {connectionPayload}')
     return ClientConnection(
         url='ws://localhost:8000', # mock_server.py
         #url='ws://localhost:4000', # satori server? 
