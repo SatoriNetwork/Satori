bda0f87965a88a7086f6eacf8bdb87540dd1974d
Jordan Miller <Jordan.kay@gmail.com>
2023-01-04 19:17:47 -0700

    commit

--- client/tests/manual/server.py
+++ client/tests/manual/server.py
@@ -12,10 +12,10 @@ class fixtures():
     def streams():
         return {
             0: {},
-            1: {'source': 'SATORI', 'pubkey': '22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'stream1', 'target': 'target'},
-            2: {'source': 'SATORI', 'pubkey': '02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'stream1_p', 'target': 'target'},
-            3: {'source': 'SATORI', 'pubkey': '32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'stream3', 'target': 'target'},
-            4: {'source': 'SATORI', 'pubkey': '42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'stream4', 'target': 'target'},
+            1: {'source': 'satori', 'pubkey': '22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'stream1', 'target': 'target', 'cadence': None, 'offset': None, 'datatype': None, 'uri': None, 'description': 'raw data'},
+            2: {'source': 'satori', 'pubkey': '02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'stream1_p', 'target': 'target', 'cadence': None, 'offset': None, 'datatype': None, 'uri': None,  'description': 'prediction on stream1'},
+            3: {'source': 'satori', 'pubkey': '32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'stream3', 'target': 'target', 'cadence': None, 'offset': None, 'datatype': None, 'uri': None, 'description': None},
+            4: {'source': 'satori', 'pubkey': '42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8', 'stream': 'gold4h', 'target': 'close',  'cadence': 60*60*4, 'offset': 60*60*1, 'datatype': 'float', 'uri': 'www.stocks.yahoo.com/gold4h', 'description': '4hr price of Gold'},
         }
 
     @staticmethod
@@ -167,6 +167,14 @@ def my_subscriptions_pins():
     print(r.status_code, r.text)
 
 
+def get_key():
+    ''' subscribe to primary data stream and and publish prediction '''
+    r = requests.get(
+        'http://localhost:5002/get/key',
+        headers=w.authPayload(asDict=True))
+    print(r.status_code, r.text)
+
+
 def checkin():
     r = requests.post(
         'http://localhost:5002/checkin',
@@ -193,7 +201,7 @@ def checkin():
         clean=True))
 
 
-if __name__ != '__main__':
+if False:
     register_wallet()
     # now, go make a new wallet in the database manually starting with 22...
     # now, go make a new wallet in the database manually starting with 32...
@@ -206,7 +214,7 @@ if __name__ != '__main__':
     # now manually fix stream 3 in the database to point to wallet 3
     # now manually fix stream 4 in the database to point to wallet 4
     # now manually set the sanctioned to > 0 for streams 1, and 3
-    # now manually set the predicting for stream 2 to 1
+    # now manually set the predicting for stream 2 to stream 1 (id 2)
     register_subscription(1)  # primary
     register_subscription(4)  # secondary
     register_pins(2)  # on publication
@@ -224,6 +232,7 @@ if __name__ != '__main__':
     my_publications()
     my_subscriptions()
     my_subscriptions_pins()
+    get_key()
 
 if __name__ == '__main__':
     # register_wallet()
@@ -240,36 +249,23 @@ if __name__ == '__main__':
 
 # python .\client\tests\manual\server.py
 
-
 # >>> checkin()
-# 200 {"key": "75hwPignMgTlefjBM3fJZr2Z2L86ioM69frY45c01adDyr5K6GcM/U6KJ9epjRN9nlNMsD/NmTIE09gE+M66e7TdsVkIab02YwcQUStXXMeRPngOgics1tQhSL4UG9beXqHRQ9ifDDULeBspaitKT7TQR1cGShw3o+tTlsPWZ8BHTTc43qYFaMFFcXWgP/GND4931KG738VhGQYL1BraJSrgon0bvDI3WHwDsurUln7ZlvYhcRDHUreMV6oMO2/1UuynnAo8DVHe2n3dxBTrerlhlFTRBUcR7oU2KmhY7jPiUMgcl1Vty/n4592L2mE+GQmSty7NkijBLmyWmusv+E3lp341v3Vre1k7Kie+U3HeVNp96As1ZVOQx03kKgCo14KyIX3NaxDiYDP461wOPvFYtojO8c/AEVcm2p0BIgJ8urQ509D0tCO4IzZtnlOHKsS/2tf36iYc22R3nWt+zJNXR3F5MBFCyZJsEiBjahfiUMLep3UFnQHIAj9ksuYg0meDlorL4TJIOGAXONt48czRGYK/k6DKyLcVoMJZFimzAYAdUVPgdgpO8Egw9F1oloEYHjJp31VU9sYWCQQCIdrqoVVucgMdSZySRNLJTCQ9q9BCsv304gJiyvaKqX/9Tw10RVJiYYc+UeH4AZLrINTE9zDB7D893U3Na4WBMbbRofk10IsoAwKPjaMItBrwSxZUyY5s3EddMFnuw33iNmfmdT8igAZEFoygGCKvpcrJbL1DbqGY/EjMAyaG8EjqbSXHnHyIbYYoeevP7JAs/HtYJE308DaY84i81NdaqvOEA6hI4iGrigXoepzdVkSjyM9VcPeP3lDeyvvVX1YopIJjv4Ang4v+iBKScbxYOa8Fcg8ctHOe+eLikyDeeuPaRbTiQcgEVxeNEVhdZhyoFzwiCF4F4cQSqZ5HOZOFPAlFlLUyRiD0yizU+cWVSCAxS5NP4kgi+7nMrQyJNUeUzStZp1wWLKQduHQPLmKuJkMZXR+y0B0tHh1e/XrGjRRL", "subscriber.key": "LTx/yQIbQi49KSUbS4hKxSn+jJB538zmKLxBnA4Y4soV457JHPfaeA4gAWjEGziZfVYG7NxRdO7qCVEyXSguPI6Ct4XZ2pbFgJT7ZLfy//ntABBPQ9zAU9Xb7dURE0XN29YjVHLI7aT8N0cV9n6sTtFXUp+maXKBFfx/iF1+UTUU1n5l0fI9tXA2fgEaLNzRXiHGBvcv0kyYECyn5YwJuPdKtU6QPOz2ue+u60hN88PFF1Y/SnCLv4560ntYiTx6Ok2KtnAphmTzuXWBdN4wmBzGGNBRJZocsbmcrtCpwpVohgqq6BijTOwZeyFrpMjISPGY64qXnoT1no5H2z+U62Nw9HSWH9oweMleF3+Pk/T7esvoJHUf/5y6OEOP5A7ku7GVHyR4+wyw+4rmlRMUbwwLFz2Sq5Eq++3DVoCqV2SWjv++oHwohyLDZcMUlf/qicizvfQ2th32OAX3tO7mobwTfYILdHZS8RdExio1vlucix+cb7RP4AnE8er7eSDu07sYE0osnOaFUUMqAVirkZAUe0QQfqS5CWJZ+tdUw6hfyJjzLikpSyvgIAS6V5/rV9TMSxo5O8evkZJ++x53j97r5+VH2k18VbQWiiYqh0M=", "publisher.key": "75hwPignMgTlefjBM3fJZr2Z2L86ioM69frY45c01adDyr5K6GcM/U6KJ9epjRN9nlNMsD/NmTIE09gE+M66e7TdsVkIab02YwcQUStXXMeRPngOgics1tQhSL4UG9beXqHRQ9ifDDULeBspaitKT7TQR1cGShw3o+tTlsPWZ8BHTTc43qYFaMFFcXWgP/GND4931KG738VhGQYL1BraJSrgon0bvDI3WHwDsurUln7ZlvYhcRDHUreMV6oMO2/1UuynnAo8DVHe2n3dxBTrerlhlFTRBUcR7oU2KmhY7jPiUMgcl1Vty/n4592L2mE+GQmSty7NkijBLmyWmusv+E3lp341v3Vre1k7Kie+U3HeVNp96As1ZVOQx03kKgCo14KyIX3NaxDiYDP461wOPmydKTATSGcIyRMh/5vql24=", "subscriptions": "[{\"source\": \"SATORI\", \"author\": \"22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream1\", \"target\": \"target\", \"sanctioned\":
-# 1, \"tspredicting_source\": \"2022-12-23 20:43:12.022\", \"reason_author\": \"SATORI\", \"reason_stream\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"reason_target\": \"stream1_p\", \"reason_is_primary\": \"target\"},
-# {\"source\": \"SATORI\", \"author\": \"32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream3\", \"target\": \"target\", \"sanctioned\": 1, \"tspredicting_source\": \"2022-12-23 20:43:16.185\", \"reason_author\": \"SATORI\", \"reason_stream\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"reason_target\": \"stream3_p\", \"reason_is_primary\": \"target\"}, {\"source\": \"SATORI\", \"author\": \"42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream4\", \"target\": \"target\",
-# \"tspredicting_source\": \"2022-12-23 20:43:19.643\", \"reason_author\": \"SATORI\", \"reason_stream\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"reason_target\": \"stream1_p\", \"reason_is_primary\": \"target\"}]", "publications": "[{\"source\": \"SATORI\", \"author\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream1_p\", \"target\": \"target\", \"tspredicting_source\": \"2022-12-23 20:43:14.116\", \"predicting_author\": \"SATORI\", \"predicting_stream\": \"22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"predicting_target\": \"stream1\", \"reason_source\": \"target\"}, {\"source\": \"SATORI\", \"author\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream3_p\", \"target\": \"target\", \"tspredicting_source\": \"2022-12-23 20:52:02.472\", \"predicting_author\": \"SATORI\", \"predicting_stream\": \"32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"predicting_target\": \"stream3\", \"reason_source\": \"target\"}]", "pins": "[{\"ipns\": \"ipns\", \"ipfs\": \"ipfs\", \"disk\": 3, \"count\": 27, \"ts\": \"2022-12-23 22:22:22.717\", \"stream_source\": \"SATORI\", \"stream_author\": \"32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream_stream\": \"stream3\", \"stream_target\": \"target\"}]", "versions": {"server": "0.0.1", "client": "0.0.1"}}
+# 200 {"key": "75hwPignMgTlefjBM3fJZr2Z2L86ioM69frY45c01adDyr5K6GcM/U6KJ9epjRN9nlNMsD/NmTIE09gE+M66e7TdsVkIab02YwcQUStXXMeRPngOgics1tQhSL4UG9beXqHRQ9ifDDULeBspaitKT7TQR1cGShw3o+tTlsPWZ8BHTTc43qYFaMFFcXWgP/GND4931KG738VhGQYL1BraJSrgon0bvDI3WHwDsurUln7ZlvYhcRDHUreMV6oMO2/1UuynnAo8DVHe2n3dxBTrerlhlFTRBUcR7oU2KmhY7jPiUMgcl1Vty/n4592L2mE+GQmSty7NkijBLmyWmusv+E3lp341v3Vre1k7Kie+U3HeVNp96As1ZVOQx03kKgCo14KyIX3NaxDiYDP461wOPvFYtojO8c/AEVcm2p0BIgJ8urQ509D0tCO4IzZtnlOHKsS/2tf36iYc22R3nWt+zJNXR3F5MBFCyZJsEiBjahfiUMLep3UFnQHIAj9ksuYg0meDlorL4TJIOGAXONt48czRGYK/k6DKyLcVoMJZFimzAYAdUVPgdgpO8Egw9F1ommBFEN12IK3f4/Q1aId+c2XR3PC5yqQVFHODsPw4Js20YrqxK9DVpbyNiziOlQMP4wnCyvCTAY1WwRZgrPlnB/GoRXsQq31asHOjyFxrGB7fHcsqf6SvjA5bwhOAPgc3rDj3qW/lDeF9kRNlQdFlvL0BmpnkZ/De9VVpiKrB3l5olR13TCBwg4DoDuXxaJFEeyKehfCtlqW6nISwB3S2VqomB7sl0aGloSepzBG2E91llr4WrT/FB+E6GmxPmdxHludzjlgVhZ2lpcHUMDf1N7I+JvJfDu1bLdhvwiIb+Y9bamyfIkDXK47i3Utzryf5PYyXieCjDBcuVLcSuNYAe9y2Qu8zPGjkqV1g3peFi4h8ghoo1FSTiAQX2nb1OOf3QJ70VJNrPUujemz29UMu58ntrGbNdCdxICAaWmImg5M=", "subscriptions": "[{\"source\": \"SATORI\", \"author\": \"22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream1\", \"target\": \"target\", \"sanctioned\": 1, \"datatype\": \"2023-01-05 00:12:27.258\", \"tspredicting_source\": \"SATORI\", \"predicting_author\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"predicting_stream\": \"stream1_p\", \"predicting_target\":
+# \"target\", \"reason_source\": true}, {\"source\": \"SATORI\", \"author\": \"32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream3\", \"target\": \"target\", \"sanctioned\": 1, \"datatype\": \"2023-01-05 00:12:32.880\", \"tspredicting_source\": \"SATORI\", \"predicting_author\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"predicting_stream\": \"stream3_p\", \"predicting_target\": \"target\", \"reason_source\": true}, {\"source\": \"SATORI\", \"author\": \"42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"gold4h\", \"target\": \"close\", \"datatype\": \"2023-01-05 00:12:35.762\", \"tspredicting_source\": \"SATORI\", \"predicting_author\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"predicting_stream\": \"stream1_p\",
+# \"predicting_target\": \"target\", \"reason_source\": false}]", "publications": "[{\"source\": \"SATORI\", \"author\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream1_p\", \"target\": \"target\", \"datatype\": \"2023-01-05 00:12:29.994\", \"description\": \"SATORI\", \"uri\": \"22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"cadence\": \"stream1\", \"offset\": \"target\", \"reason_source\": false}, {\"source\": \"SATORI\", \"author\": \"02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream\": \"stream3_p\", \"target\": \"target\", \"datatype\": \"2023-01-05 01:10:09.876\", \"description\": \"SATORI\", \"uri\": \"32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"cadence\": \"stream3\", \"offset\": \"target\", \"reason_source\": false}]", "pins": "[{\"ipns\": \"ipns\", \"ipfs\": \"ipfs\", \"disk\": 3, \"count\": 27, \"ts\": \"2023-01-05 01:06:44.399\", \"stream_source\": \"SATORI\", \"stream_author\": \"32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8\", \"stream_stream\": \"stream3\", \"stream_target\": \"target\"}]", "versions": {"server": "0.0.1", "client": "0.0.1"}}
 #
-# key 75hwPignMgTlefjBM3fJZr2Z2L86ioM69frY45c01adDyr5K6GcM/U6KJ9epjRN9nlNMsD/NmTIE09gE+M66e7TdsVkIab02YwcQUStXXMeRPngOgics1tQhSL4UG9beXqHRQ9ifDDULeBspaitKT7TQR1cGShw3o+tTlsPWZ8BHTTc43qYFaMFFcXWgP/GND4931KG738VhGQYL1BraJSrgon0bvDI3WHwDsurUln7ZlvYhcRDHUreMV6oMO2/1UuynnAo8DVHe2n3dxBTrerlhlFTRBUcR7oU2KmhY7jPiUMgcl1Vty/n4592L2mE+GQmSty7NkijBLmyWmusv+E3lp341v3Vre1k7Kie+U3HeVNp96As1ZVOQx03kKgCo14KyIX3NaxDiYDP461wOPvFYtojO8c/AEVcm2p0BIgJ8urQ509D0tCO4IzZtnlOHKsS/2tf36iYc22R3nWt+zJNXR3F5MBFCyZJsEiBjahfiUMLep3UFnQHIAj9ksuYg0meDlorL4TJIOGAXONt48czRGYK/k6DKyLcVoMJZFimzAYAdUVPgdgpO8Egw9F1oloEYHjJp31VU9sYWCQQCIdrqoVVucgMdSZySRNLJTCQ9q9BCsv304gJiyvaKqX/9Tw10RVJiYYc+UeH4AZLrINTE9zDB7D893U3Na4WBMbbRofk10IsoAwKPjaMItBrwSxZUyY5s3EddMFnuw33iNmfmdT8igAZEFoygGCKvpcrJbL1DbqGY/EjMAyaG8EjqbSXHnHyIbYYoeevP7JAs/HtYJE308DaY84i81NdaqvOEA6hI4iGrigXoepzdVkSjyM9VcPeP3lDeyvvVX1YopIJjv4Ang4v+iBKScbxYOa8Fcg8ctHOe+eLikyDeeuPaRbTiQcgEVxeNEVhdZhyoFzwiCF4F4cQSqZ5HOZOFPAlFlLUyRiD0yizU+cWVSCAxS5NP4kgi+7nMrQyJNUeUzStZp1wWLKQduHQPLmKuJkMZXR+y0B0tHh1e/XrGjRRL
+# key 75hwPignMgTlefjBM3fJZr2Z2L86ioM69frY45c01adDyr5K6GcM/U6KJ9epjRN9nlNMsD/NmTIE09gE+M66e7TdsVkIab02YwcQUStXXMeRPngOgics1tQhSL4UG9beXqHRQ9ifDDULeBspaitKT7TQR1cGShw3o+tTlsPWZ8BHTTc43qYFaMFFcXWgP/GND4931KG738VhGQYL1BraJSrgon0bvDI3WHwDsurUln7ZlvYhcRDHUreMV6oMO2/1UuynnAo8DVHe2n3dxBTrerlhlFTRBUcR7oU2KmhY7jPiUMgcl1Vty/n4592L2mE+GQmSty7NkijBLmyWmusv+E3lp341v3Vre1k7Kie+U3HeVNp96As1ZVOQx03kKgCo14KyIX3NaxDiYDP461wOPvFYtojO8c/AEVcm2p0BIgJ8urQ509D0tCO4IzZtnlOHKsS/2tf36iYc22R3nWt+zJNXR3F5MBFCyZJsEiBjahfiUMLep3UFnQHIAj9ksuYg0meDlorL4TJIOGAXONt48czRGYK/k6DKyLcVoMJZFimzAYAdUVPgdgpO8Egw9F1ommBFEN12IK3f4/Q1aId+c2XR3PC5yqQVFHODsPw4Js20YrqxK9DVpbyNiziOlQMP4wnCyvCTAY1WwRZgrPlnB/GoRXsQq31asHOjyFxrGB7fHcsqf6SvjA5bwhOAPgc3rDj3qW/lDeF9kRNlQdFlvL0BmpnkZ/De9VVpiKrB3l5olR13TCBwg4DoDuXxaJFEeyKehfCtlqW6nISwB3S2VqomB7sl0aGloSepzBG2E91llr4WrT/FB+E6GmxPmdxHludzjlgVhZ2lpcHUMDf1N7I+JvJfDu1bLdhvwiIb+Y9bamyfIkDXK47i3Utzryf5PYyXieCjDBcuVLcSuNYAe9y2Qu8zPGjkqV1g3peFi4h8ghoo1FSTiAQX2nb1OOf3QJ70VJNrPUujemz29UMu58ntrGbNdCdxICAaWmImg5M=
 #
-# subscriptions [
-#   {"source": "SATORI", "author": "22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1", "target": "target", "sanctioned": 1, "tspredicting_source": "2022-12-23 20:43:12.022", "reason_author": "SATORI", "reason_stream": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "reason_target": "stream1_p", "reason_is_primary": "target"},
-# 	{"source": "SATORI", "author": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3", "target": "target", "sanctioned": 1, "tspredicting_source": "2022-12-23 20:43:16.185", "reason_author": "SATORI", "reason_stream": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "reason_target": "stream3_p", "reason_is_primary": "target"},
-# 	{"source": "SATORI", "author": "42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream4", "target": "target", "tspredicting_source": "2022-12-23 20:43:19.643", "reason_author": "SATORI", "reason_stream": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "reason_target": "stream1_p", "reason_is_primary": "target"}]
+# subscriptions [{"source": "SATORI", "author": "22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1", "target": "target", "sanctioned": 1, "datatype": "2023-01-05
+# 00:12:27.258", "tspredicting_source": "SATORI", "predicting_author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "predicting_stream": "stream1_p", "predicting_target": "target", "reason_source": true}, {"source": "SATORI", "author": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3", "target": "target", "sanctioned": 1, "datatype": "2023-01-05 00:12:32.880", "tspredicting_source": "SATORI", "predicting_author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "predicting_stream": "stream3_p", "predicting_target": "target", "reason_source": true}, {"source": "SATORI",
+# "author": "42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "gold4h", "target": "close", "datatype": "2023-01-05 00:12:35.762", "tspredicting_source": "SATORI", "predicting_author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "predicting_stream": "stream1_p", "predicting_target": "target", "reason_source": false}]
 #
-# publications [
-# 	{"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1_p", "target": "target", "tspredicting_source": "2022-12-23 20:43:14.116", "predicting_author": "SATORI", "predicting_stream": "22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "predicting_target": "stream1", "reason_source": "target"},
-# 	{"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3_p", "target": "target", "tspredicting_source": "2022-12-23 20:52:02.472", "predicting_author": "SATORI", "predicting_stream": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "predicting_target": "stream3", "reason_source": "target"}]
+# publications [{"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1_p", "target": "target", "datatype": "2023-01-05 00:12:29.994", "description": "SATORI", "uri": "22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "cadence": "stream1", "offset": "target", "reason_source": false}, {"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3_p", "target": "target", "datatype": "2023-01-05 01:10:09.876", "description": "SATORI", "uri": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "cadence": "stream3", "offset": "target", "reason_source": false}]
 #
-# pins [
-# {"ipns": "ipns", "ipfs": "ipfs", "disk": 3, "count": 27, "ts": "2022-12-23 22:22:22.717", "stream_source": "SATORI", "stream_author": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream_stream": "stream3", "stream_target": "target"}]
+# pins [{"ipns": "ipns", "ipfs": "ipfs", "disk": 3, "count": 27, "ts": "2023-01-05 01:06:44.399", "stream_source": "SATORI", "stream_author": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream_stream": "stream3", "stream_target": "target"}]
 #
 # server version 0.0.1
 #
 # client version 0.0.1
 #
-# key
-# {"publisher": [
-# 	{"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1_p", "target": "target"},
-# 	{"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3_p", "target": "target"}],
-# "subscriptions": [
-# 	{"source": "SATORI", "author": "42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream4", "target": "target"},
-# 	{"source": "SATORI", "author": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3", "target": "target"},
-# 	{"source": "SATORI", "author": "22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1", "target": "target"}]}
+# key {"publisher": [{"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1_p", "target": "target"}, {"source": "SATORI", "author": "02a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3_p", "target": "target"}], "subscriptions": [{"source": "SATORI", "author": "42a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "gold4h", "target": "close"}, {"source": "SATORI", "author": "22a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream1", "target": "target"}, {"source": "SATORI", "author": "32a85fb71485c6d7c62a3784c5549bd3849d0afa3ee44ce3f9ea5541e4c56402d8", "stream": "stream3", "target": "target"}]}
